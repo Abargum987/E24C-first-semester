2ab32f6f439b78edd5e80b7541571727810e5a93
Abargum <andreasbargum1@hotmail.com>
2025-03-14 09:38:45 +0100

    su

--- javascript-intro/js/js.js
+++ javascript-intro/js/js.js
@@ -1,5 +1,9 @@
-/**
- * file: js/js.js
- * purpose: Behaviors
- **/
-console.log('Success: JavaScript running!')
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
